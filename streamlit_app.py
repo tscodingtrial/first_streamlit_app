5d045105db60910d2c6d02d65036e74cdecfc7e0
tscodingtrial <139878599+tscodingtrial@users.noreply.github.com>
2023-07-25 11:21:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-  return frutiyvice_normalized
+  return fruityvice_normalized
 
 
 #New section to display fruityvice api response
